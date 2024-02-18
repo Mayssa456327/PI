9ffd04d71c1efe67e60dd3a4310b62987eed0903
Mayssa456327 <mayssa.benhammouda@esprit.tn>
2024-02-18 18:49:32 +0100

    first commit by mayssa

---
+++ migrations/Version20240211185347.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240211185347 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement DROP FOREIGN KEY FK_B26681EA76ED395');
+        $this->addSql('DROP INDEX IDX_B26681EA76ED395 ON evenement');
+        $this->addSql('ALTER TABLE evenement DROP user_id');
+        $this->addSql('ALTER TABLE participant ADD user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE participant ADD CONSTRAINT FK_D79F6B11A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_D79F6B11A76ED395 ON participant (user_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement ADD user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE evenement ADD CONSTRAINT FK_B26681EA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_B26681EA76ED395 ON evenement (user_id)');
+        $this->addSql('ALTER TABLE participant DROP FOREIGN KEY FK_D79F6B11A76ED395');
+        $this->addSql('DROP INDEX IDX_D79F6B11A76ED395 ON participant');
+        $this->addSql('ALTER TABLE participant DROP user_id');
+    }
+}
